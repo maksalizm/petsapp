476e6beb584e46796ede38dc5c87adf0bd85cd7d
pms <pms@wishket.com>
2016-11-07 17:51:23 +0900

    error fix

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -1,15 +1,22 @@
 package com.example.android.pets;
-
 /**
  * Created by maksalizm on 2016. 11. 6..
  */
+import android.database.Cursor;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.example.android.pets.data.PetContract.PetEntry;
+
+/**
+ * Allows user to create a new pet or edit an existing one.
+ */
 
 import android.content.Context;
-import android.database.Cursor;
-import android.view.View;
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
-
+import android.widget.TextView;
 
 /**
  * {@link PetCursorAdapter} is an adapter for a list or grid view
@@ -39,8 +46,8 @@ public class PetCursorAdapter extends CursorAdapter {
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        // TODO: Fill out this method and return the list item view (instead of null)
-        return null;
+        // Inflate a list item view using the layout specified in list_item.xml
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
     }
 
     /**
@@ -55,6 +62,27 @@ public class PetCursorAdapter extends CursorAdapter {
      */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        // TODO: Fill out this method
+        // Find individual views that we want to modify in the list item layout
+        TextView nameTextView = (TextView) view.findViewById(R.id.name);
+        TextView summaryTextView = (TextView) view.findViewById(R.id.summary);
+
+        // Find the columns of pet attributes that we're interested in
+        int nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME);
+        int breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED);
+
+        // Read the pet attributes from the Cursor for the current pet
+        String petName = cursor.getString(nameColumnIndex);
+        String petBreed = cursor.getString(breedColumnIndex);
+
+        // If the pet breed is empty string or null, then use some default text
+        // that says "Unknown breed", so the TextView isn't blank.
+        if (TextUtils.isEmpty(petBreed)) {
+            petBreed = context.getString(R.string.unknown_breed);
+        }
+
+        // Update the TextViews with the attributes for the current pet
+        nameTextView.setText(petName);
+        summaryTextView.setText(petBreed);
     }
 }
+
